7d361fd08d6a9ed336944dc6f8eb6b5842fe90ba
noelia96-96 <noeliagalindo96@gmail.com>
2020-11-10 13:26:18 +0100

    funciones

--- script.js
+++ script.js
@@ -24,50 +24,51 @@
 
 //Toda la funcionalidad en este script
 
-window.addEventListener('load',onLoad);
+
 
 var arrayUsers = new Array(7);
 
+//Funcion crear usuarios
 function createUsers(){
    var user1 = new Object();
-    user1.nombre = 'usuario1';
+    user1.name = 'usuario1';
     user1.country = 'spain';
     user1.money = 199;
     user1.premiumAccount = 'true';
 
     var user2 = new Object();
-      user2.nombre = 'usuario2';
+      user2.name = 'usuario2';
       user2.country = 'france';
       user2.money = 0;
       user2.premiumAccount = 'false';
 
     var user3 = new Object();
-       user3.nombre = 'usuario3';
+       user3.name = 'usuario3';
        user3.country = 'spain';
        user3.money = 537;
        user3.premiumAccount = 'false';
 
 
     var user4 = new Object();
-       user4.nombre = 'usuario4';
+       user4.name = 'usuario4';
        user4.country = 'italy';
        user4.money = 1004;
        user4.premiumAccount = 'true';
    
     var user5 = new Object();
-       user5.nombre = 'usuario5';
+       user5.name = 'usuario5';
        user5.country = 'spain';
        user5.money = 240;
        user5.premiumAccount = 'false'
     
     var user6 = new Object();
-       user6.nombre = 'usuario6';
+       user6.name = 'usuario6';
        user6.country = 'ireland';
        user6.money = 799;
        user6.premiumAccount = 'true';
    
     var user7 = new Object();
-      user7.nombre = 'usuario7'
+      user7.name = 'usuario7'
       user7.country = 'spain';
       user7.money = 3345;
       user7.premiumAccount = 'false';
@@ -75,9 +76,23 @@ function createUsers(){
       arrayUsers = [user1,user2, user3, user4, user5, user6, user7];
 }
 
+//Funcion de filtrado 
+var filterArrayUsers;
+function filterUsers(arrayUsers){
+
+  for(let i=0; i < arrayUsers.length; i++){
+    if(arrayUsers[i].money >200){
+      filterArrayUsers.push(arrayUsers[i]);
+    }
+  }
+  return filterArrayUsers;
+
+}
+
+window.addEventListener('load',onLoad);
+
 function onLoad(){
 
- 
-
-  console.log('hi')
+console.log(arrayUsers);
+console.log(filterArrayUsers);
 }
